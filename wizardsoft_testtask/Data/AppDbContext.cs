d87308e8cf9967ee71447d40069d5488f7e3758a
maxsmg <maxsmg@mail.ru>
2026-02-05 16:52:40 +0700

    Add DB connection & model

---
+++ wizardsoft_testtask/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wizardsoft_testtask.Models;
+
+namespace wizardsoft_testtask.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TreeNode> TreeNodes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TreeNode>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.Property(x => x.Name).IsRequired().HasMaxLength(200);
+                entity.HasOne(x => x.Parent)
+                    .WithMany(x => x.Children)
+                    .HasForeignKey(x => x.ParentId)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+}
---
+++ wizardsoft_testtask/Models/TreeNode.cs
@@ -0,0 +1,11 @@
+﻿namespace wizardsoft_testtask.Models
+{
+    public class TreeNode
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public long? ParentId { get; set; }
+        public TreeNode? Parent { get; set; }
+        public ICollection<TreeNode> Children { get; set; } = new List<TreeNode>();
+    }
+}
